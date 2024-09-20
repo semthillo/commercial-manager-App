addb1eecb102642f74409db4f0b5da745ed6adf2
thillo <salysem90@gmail.com>
2024-09-20 10:26:29 +0200

    Corrected active menu in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,39 @@
-<template >
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container ">
-    <router-link class="navbar-brand" to="/"><i class="fa fa-home" aria-hidden="true"></i> Home</router-link>
-    <div class="collapse navbar-collapse justify-content-end"  id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item me-4">
-          <router-link class="nav-link" to="/list-customer"><i class="fa fa-users" aria-hidden="true"></i> Customers</router-link>
-        </li>
-        <li class="nav-item me-4">
-          <router-link class="nav-link" to="/list-product" ><i class="fa fa-cube" aria-hidden="true"></i> Products</router-link>
-        </li>
-        <li class="nav-item me-4 active">
-          <router-link class="nav-link" to="/list-order" ><i class="fa fa-shopping-basket" aria-hidden="true"></i> Orders</router-link>
-        </li>
-      </ul>
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <router-link class="navbar-brand" to="/"><i class="fa fa-home" aria-hidden="true"></i> Home</router-link>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item me-4">
+            <router-link class="nav-link" to="/list-customer">
+              <i class="fa fa-users" aria-hidden="true"></i> Customers
+            </router-link>
+          </li>
+          <li class="nav-item me-4">
+            <router-link class="nav-link" to="/list-product">
+              <i class="fa fa-cube" aria-hidden="true"></i> Products
+            </router-link>
+          </li>
+          <li class="nav-item me-4">
+            <router-link class="nav-link" to="/list-order">
+              <i class="fa fa-shopping-basket" aria-hidden="true"></i> Orders
+            </router-link>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
-<script setup>
 
+<script setup>
+// Pas besoin de script pour gérer les liens actifs.
 </script>
 
-
 <style scoped>
- 
+/* Appliquer le style à la classe générée par Vue Router */
+.router-link-active {
+  color: white;
+  border-bottom: 5px  solid whitesmoke;
+  
+}
 </style>
